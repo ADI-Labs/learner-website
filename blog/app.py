f0d3a39c8031c57f858d92a32cf909e7732b4d47
Lesley <lc2958@columbia.edu>
2019-01-01 01:02:09 -0500

    cleaned up app.py

--- blog/app.py
+++ blog/app.py
@@ -82,7 +82,7 @@ class Entry(flask_db.Model):
     def save(self, *args, **kwargs):
         # Generate a URL-friendly representation of the entry's title.
         if not self.slug:
-            self.slug = re.sub('[^\w]+', '-', self.title.lower()).strip('-')
+            self.slug = re.sub(r'[^\w]+', '-', self.title.lower()).strip('-')
         ret = super(Entry, self).save(*args, **kwargs)
 
         # Store search content.
@@ -110,11 +110,11 @@ class Entry(flask_db.Model):
 
     @classmethod
     def public(cls):
-        return Entry.select().where(Entry.published == True)
+        return Entry.select().where(Entry.published)
 
     @classmethod
     def drafts(cls):
-        return Entry.select().where(Entry.published == False)
+        return Entry.select().where(Entry.published is False)
 
     @classmethod
     def search(cls, query):
@@ -133,14 +133,16 @@ class Entry(flask_db.Model):
                 .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
                 .where(
                     FTSEntry.match(search) &
-                    (Entry.published == True))
+                    (Entry.published))
                 .order_by(SQL('score')))
+
 
 class FTSEntry(FTSModel):
     content = TextField()
 
     class Meta:
         database = database
+
 
 def login_required(fn):
     @functools.wraps(fn)
@@ -149,6 +151,7 @@ def login_required(fn):
             return fn(*args, **kwargs)
         return redirect(url_for('login', next=request.path))
     return inner
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -161,10 +164,12 @@ def login():
             session['logged_in'] = True
             session.permanent = True  # Use cookie to store session.
             flash('You are now logged in.', 'success')
-            return redirect(next_url or url_for('logged_in') or url_for('index'))
+            return redirect(next_url or url_for(
+                'logged_in') or url_for('index'))
         else:
             flash('Incorrect password.', 'danger')
     return render_template('login.html', next_url=next_url)
+
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
@@ -172,6 +177,7 @@ def logout():
         session.clear()
         return redirect(url_for('login'))
     return render_template('logout.html')
+
 
 @app.route('/loggedin/')
 @login_required
@@ -192,6 +198,7 @@ def logged_in():
         search=search_query,
         check_bounds=False)
 
+
 @app.route('/about/', methods=['GET', 'POST'])
 def about():
     if request.method == 'POST':
@@ -199,12 +206,14 @@ def about():
         return redirect(url_for('about'))
     return render_template('about.html')
 
+
 @app.route('/contact/', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
         session.clear()
         return redirect(url_for('contact'))
     return render_template('contact.html')
+
 
 @app.route('/')
 def index():
@@ -223,6 +232,7 @@ def index():
         query,
         search=search_query,
         check_bounds=False)
+
 
 def _create_or_edit(entry, template):
     if request.method == 'POST':
@@ -248,16 +258,19 @@ def _create_or_edit(entry, template):
 
     return render_template(template, entry=entry)
 
+
 @app.route('/create/', methods=['GET', 'POST'])
 @login_required
 def create():
     return _create_or_edit(Entry(title='', content=''), 'create.html')
+
 
 @app.route('/drafts/')
 @login_required
 def drafts():
     query = Entry.drafts().order_by(Entry.timestamp.desc())
     return object_list('index.html', query, check_bounds=False)
+
 
 @app.route('/<slug>/')
 def detail(slug):
@@ -268,11 +281,13 @@ def detail(slug):
     entry = get_object_or_404(query, Entry.slug == slug)
     return render_template('detail.html', entry=entry)
 
+
 @app.route('/<slug>/edit/', methods=['GET', 'POST'])
 @login_required
 def edit(slug):
     entry = get_object_or_404(Entry, Entry.slug == slug)
     return _create_or_edit(entry, 'edit.html')
+
 
 @app.template_filter('clean_querystring')
 def clean_querystring(request_args, *keys_to_remove, **new_values):
@@ -287,13 +302,16 @@ def clean_querystring(request_args, *keys_to_remove, **new_values):
     querystring.update(new_values)
     return urllib.urlencode(querystring)
 
+
 @app.errorhandler(404)
 def not_found(exc):
     return Response('<h3>Not found</h3>'), 404
 
+
 def main():
     database.create_tables([Entry, FTSEntry], safe=True)
     app.run(debug=True)
+
 
 if __name__ == '__main__':
     main()
